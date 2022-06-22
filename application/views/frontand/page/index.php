6f9455f8850a75e3a48bec5809074beb1c085a1d
johan santri <johansantri@live.com>
2022-06-22 15:13:52 +0700

    update image
    thumbnail index

--- application/views/frontand/page/index.php
+++ application/views/frontand/page/index.php
@@ -272,11 +272,11 @@
                     <span class="font-darker-grotesque mt-6 block">oleh <span class="font-bold">admin</span></span>
                 </div>
                 <div class="-mt-7">
-                    <img class="w-full h-48 rounded-[20px] object-cover object-center" src="<?php echo base_url()?>upload/<?php echo $key->image;?>" alt="<?php echo $key->title;?>">
+                    <img style="width:100%; height:200px; border-radius: 5%;" src="<?php echo base_url()?>upload/<?php echo $key->image;?>" alt="<?php echo $key->title;?>">
                 </div>
                 <div class="px-6 bg-gray-50 -mt-4 pt-10 pb-6 rounded-b-[1.35rem]">
                     <p class="font-darker-grotesque h-24"><?php echo substr($key->meta, 0,80). '...'?></p>
-                    <a href="<?php echo base_url()?>berita/<?php echo $key->slug_title;?>" class="font-darker-grotesque font-bold block mt-10">Baca Selengkapnya <img src="chevron-right-icon.png" alt=""></a>
+                    <a href="<?php echo base_url()?>berita/<?php echo $key->slug_title;?>" class="font-darker-grotesque font-bold block mt-10">Baca Selengkapnya </a>
                 </div>          
             
             </div>    
