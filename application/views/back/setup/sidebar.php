d41e537d4e6a92bc750f9871225fc2a659962776
johan santri <johansantri@live.com>
2022-06-24 09:54:57 +0700

    update back sidebar link
    update link sidebar

--- application/views/back/setup/sidebar.php
+++ application/views/back/setup/sidebar.php
@@ -7,9 +7,9 @@
         </div>
       </div>
       <ul class="app-menu">
-        <li><a class="app-menu__item active" href="dashboard.html"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+        <li><a class="app-menu__item active" href="<?php echo base_url()?>dasbord"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
        
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">Master Data</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <li class="treeview"><a class="app-menu__item" href="<?php echo base_url()?>dasbord" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">Master Data</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
             <li><a class="treeview-item" href="<?php echo base_url()?>kategori"><i class="app-menu__icon fa fa-edit"></i> Kategori</a></li>
             <li><a class="treeview-item" href="<?php echo base_url()?>sub"  rel="noopener"><i class="app-menu__icon fa fa-edit"></i> Sub Kategori</a></li>
