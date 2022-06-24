e391e681cecded8aabd75505eb0678ce7c58f949
johan santri <johansantri@live.com>
2022-06-24 09:48:33 +0700

    back dasbord
    update title

--- application/views/back/page/dasbord.php
+++ application/views/back/page/dasbord.php
@@ -13,7 +13,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
             <div class="info">
-              <h4>Users</h4>
+              <h4>Kegiatan</h4>
               <p><b>5</b></p>
             </div>
           </div>
@@ -21,7 +21,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
             <div class="info">
-              <h4>Likes</h4>
+              <h4>Berita</h4>
               <p><b>25</b></p>
             </div>
           </div>
@@ -29,7 +29,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small warning coloured-icon"><i class="icon fa fa-files-o fa-3x"></i>
             <div class="info">
-              <h4>Uploades</h4>
+              <h4>Unduh</h4>
               <p><b>10</b></p>
             </div>
           </div>
@@ -37,7 +37,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small danger coloured-icon"><i class="icon fa fa-star fa-3x"></i>
             <div class="info">
-              <h4>Stars</h4>
+              <h4>Panduan</h4>
               <p><b>500</b></p>
             </div>
           </div>
