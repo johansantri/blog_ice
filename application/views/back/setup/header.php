8aa8674ea8f6aa1a4500448ebab24f40910fdc9e
johan santri <johansantri@live.com>
2022-06-24 09:53:47 +0700

    update back header
    link update n logo

--- application/views/back/setup/header.php
+++ application/views/back/setup/header.php
@@ -1,4 +1,4 @@
- <header class="app-header"><a class="app-header__logo" href="index.html">Vali</a>
+ <header class="app-header"><a class="app-header__logo" href="<?php echo base_url()?>dasbord"><img src="https://icei.ac.id/static/comprehensive/images/logo.3a535928f907.png" style="width: 150px;" alt=""></a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
@@ -7,10 +7,10 @@
         </li>
        
         <!-- User Menu-->
-        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
+        <li class="dropdown"><a class="app-nav__item" href="<?php echo base_url()?>dasbord" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
           <ul class="dropdown-menu settings-menu dropdown-menu-right">
-            <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
-            <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li>
+            <li><a class="dropdown-item" href="#"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
+            <li><a class="dropdown-item" href="#"><i class="fa fa-user fa-lg"></i> Profile</a></li>
             <li><a class="dropdown-item" href="<?php echo base_url()?>keluar"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
           </ul>
         </li>
